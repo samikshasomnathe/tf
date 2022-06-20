dc80dfd3a063b63f033905fa9cc378fb149322e3
samiksha <samikshasomnthe2000@gmail.com>
2022-06-20 17:00:20 +0530

    added inverted1

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -29,7 +29,7 @@ resource "aws_internet_getway" "igw" {
 resource "aws_default_route_table" "igw"{
     default_route_table_id = aws_vpc.vpc1.default_route_table_id
     route{
-        cidr_block = 0.0.0.0/0
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_vpc.igw.id
 
     }
