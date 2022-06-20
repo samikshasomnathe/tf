ae7be27f21e47fdb136e1dcbea0e104dc502fda9
samiksha <samikshasomnthe2000@gmail.com>
2022-06-20 16:55:16 +0530

    added inverted

--- variable.tf
+++ variable.tf
@@ -1,15 +1,15 @@
 variable "cidr_vpc"{
-    default = 10.0.0.0/16
+    default = "10.0.0.0/16"
 }
 variable "cidr_private" {
-  default =10.0.0.0/20
+  default ="10.0.0.0/20"
 }
 variable "zone_private" {
     default="ap-south-1a"
   
 }
 variable "cidr_public" {
-    default=10.0.16.0/20
+    default="10.0.16.0/20"
   
 }
 variable "zone_public" {
